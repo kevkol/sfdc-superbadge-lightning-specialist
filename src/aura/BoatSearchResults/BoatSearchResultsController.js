93c945f7cbc8dffb4e630897f9ffa1f36859d2fe
Jacky Zhao <liang.zhao83@gmail.com>
2018-02-07 15:25:00 -0800

    Populate the Search Results

--- src/aura/BoatSearchResults/BoatSearchResultsController.js
+++ src/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -1,4 +1,12 @@
 ({
-	myAction : function(component, event, helper) {
-	}
+
+  doInit: function(component, event, helper) {
+    helper.onSearch(component);
+  },
+
+  // doSearch: function (component, event, helper) {
+  //     var params = event.getParam('arguments');
+  //     component.set("v.boatTypeId", params.boatTypeId);
+  //     helper.onSearch(component);
+  // }
 })
--- src/aura/BoatSearchResults/BoatSearchResultsHelper.js
+++ src/aura/BoatSearchResults/BoatSearchResultsHelper.js
@@ -1,4 +1,25 @@
 ({
-	helperMethod : function() {
-	}
+  onSearch: function(component) {
+
+    var action = component.get("c.getBoats");
+    action.setParams({
+      boatTypeId: component.get("v.boatTypeId")
+    });
+
+    action.setCallback(this, function(response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        var boats = response.getReturnValue();
+        component.set("v.boats", boats);
+
+        if (boats.length == 0) {
+          component.set("v.noBoatsFlag", true);
+        } else {
+          component.set("v.noBoatsFlag", false);
+        }
+      }
+    });
+
+    $A.enqueueAction(action);
+  }
 })
