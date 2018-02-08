9cd7c990f0319c4844c404d4b7c8289febbad725
Jacky Zhao <liang.zhao83@gmail.com>
2018-02-08 13:24:09 -0800

    Highlight the Selected Boat

--- src/aura/BoatSearchResults/BoatSearchResultsController.js
+++ src/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -8,5 +8,9 @@
     var params = event.getParam('arguments');
     component.set("v.boatTypeId", params.boatTypeId);
     helper.onSearch(component);
+  },
+  onBoatSelect: function(component, event, helper) {
+    var boatId = event.getParam("boatId");
+    component.set("v.selectedBoatId", boatId);
   }
 })
--- src/aura/BoatTile/BoatTileController.js
+++ src/aura/BoatTile/BoatTileController.js
@@ -1,4 +1,10 @@
 ({
-	myAction : function(component, event, helper) {
-	}
+  onBoatClick: function(component, event, helper) {
+    var selectBoat = component.getEvent("BoatSelect");
+    var boatId = component.get("v.boat.Id");
+    selectBoat.setParams({
+      "boatId": boatId
+    });
+    selectBoat.fire();
+  }
 })
