986d0f9903f7efa0698d713e083419d94f332d02
Jacky Zhao <liang.zhao83@gmail.com>
2018-02-09 15:27:34 -0800

    refresh from server

--- src/aura/BoatDetails/BoatDetailsController.js
+++ src/aura/BoatDetails/BoatDetailsController.js
@@ -6,9 +6,16 @@
     service.reloadRecord();
   },
   onRecordUpdated: function(component, event, helper) {
-
+    var boatReviewsCmp = component.find("boatReviewsCmp");
+    if (boatReviewsCmp) {
+      boatReviewsCmp.refresh();
+    }
   },
   onBoatReviewAdded: function(component, event, helper) {
+    var boatReviewsCmp = component.find("boatReviewsCmp");
+    if (boatReviewsCmp) {
+      boatReviewsCmp.refresh();
+    }
     component.find("tabs").set("v.selectedTabId", "boatreviewtab");
   }
 })
